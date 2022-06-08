ab52c2d524b0a8bf474167d760659bca22e714d6
jichan <joseph@jc-lab.net>
2022-06-08 17:12:01 +0900

    fix

--- docker/start-watch.sh
+++ docker/start-watch.sh
@@ -16,12 +16,20 @@ update_tls() {
 	cat ${FRONTEND_TLS_PATH}/tls.key ${FRONTEND_TLS_PATH}/tls.crt >> ${FRONTEND_TLS_OUT}
 }
 
+find_pid() {
+	ps -a | grep ' haproxy ' | grep -v grep | sed -E 's/^\s*//g' | cut -d' ' -f1
+}
+
 update_tls
 
-until [ -f /var/run/haproxy/haproxy.pid ]; do
+#until [ -f /var/run/haproxy/haproxy.pid ]; do
+until find_pid; do
 	log "waiting for ready haproxy"
 	sleep 1
 done
+
+#haproxy_pid=$(cat /var/run/haproxy/haproxy.pid)
+haproxy_pid=$(find_pid)
 
 inotifywait -r -q -e modify,move,create,delete --format '%w %e %T' -m --timefmt '%H%M%S' \
 ${FRONTEND_TLS_PATH} | while read file event tm; do
@@ -31,7 +39,7 @@ ${FRONTEND_TLS_PATH} | while read file event tm; do
 	if [ $delta -lt 2 -a $delta -gt -2 ] ; then
 		sleep 1
 		update_tls
-		kill -HUP $(cat /var/run/haproxy/haproxy.pid)
+		kill -HUP ${haproxy_pid}
 	fi
 done
 
